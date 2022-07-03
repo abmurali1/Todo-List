e0ff5a7c0a7d8cce946998f7bcb79baabff4ff3c
Balamurali Arumugam <abmurali18@gmail.com>
2022-07-03 17:19:59 +0530

    Test case commit

--- src/components/templates/HomePage/test/HomePage.test.tsx
+++ src/components/templates/HomePage/test/HomePage.test.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import HomePage from '../index';
 
-describe('<Button>', () => {
+describe('<Home>', () => {
   it('renders correctly', () => {
-    const tree = renderer.create(<HomePage title="Coin Market" />).toJSON();
+    const tree = renderer
+      .create(<HomePage title="Coin Market" navigation={jest.fn()} />)
+      .toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
